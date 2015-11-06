d1d042f9e81651de08d8c13e2f6d2427367c88da
Ryan <ryanhuebert@gmail.com>
2015-11-06 12:40:15 -0800

    Lecture 5 Completed

---
+++ lectures/lecture5-happiness/Happiness/HappinessViewController.swift
@@ -0,0 +1,14 @@
+//
+//  HappinessViewController.swift
+//  Happiness
+//
+//  Created by Ryan Huebert on 11/6/15.
+//  Copyright © 2015 Ryan Huebert. All rights reserved.
+//
+
+import UIKit
+
+class HappinessViewController: UIViewController {
+
+    
+}
